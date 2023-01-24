b16a429157f4163cf36bf6067ff848321b27cbe7
Yohan Daza <yohangr3@hotmail.com>
2023-01-24 10:56:41 -0500

    Exercises easiest

---
+++ src/Exercise1/ThirdAngle.java
@@ -0,0 +1,11 @@
+package Exercise1;
+
+public class ThirdAngle {
+    public static int otherAngle(int angle1, int angle2) {
+        // TODO: Place code here
+        return  180- angle1 - angle2;
+
+
+
+    }
+}
---
+++ src/Exercise2/Sid.java
@@ -0,0 +1,10 @@
+package Exercise2;
+
+public class Sid {
+    public static int simpleMultiplication(int n) {
+        if(n % 2 == 0){
+            return n * 8;
+        }
+        return n*9;
+    }
+}
