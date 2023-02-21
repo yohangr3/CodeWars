7b2e3b4bcfcfe1af4cf1d11c1ae4e39fb75d0ef2
Yohan Daza <yohangr3@hotmail.com>
2023-02-21 11:14:50 -0500

    Final kata nivel 8

--- src/main.java
+++ src/main.java
@@ -3,19 +3,10 @@ import java.util.*;
 class main {
 
     public static void main(String[] args) {
-        int[] n = {1, 2, 3, 4};
-        int num = 4;
 
-
-        for (int i = 0; i < n.length; i++) {
-            if(num > n.length-1) {
-                System.out.println("-1");
-            } else if (num == i) {
-                n[i] = (int)Math.pow(n[i],num);
-                System.out.println(n[i]);
-            }
-        }
     }
+
+
 
 
 }
